067f51e68664da7f17701857825b5a42e1b3cbcd
cuiyankang <15835424939@163.com>
2019-11-12 20:54:45 +0800

    cyk

---
+++ src/pages/details/index.vue
@@ -0,0 +1,233 @@
+<template>
+    <main>
+        <div class="desc">
+            <div class="lr-outline">
+                <img src="https://bookpic.lrts.me/nibgfjr59buwsoljw6mayydru5q367yb_180x254.jpeg">
+            </div>
+            <div class="lr-tag">
+                <h4>抓住3-6岁儿童心理：陪孩子走过敏感期</h4>
+               <div class="xin">
+                <i class="iconfont icon-icon"></i>
+                <i class="iconfont icon-icon"></i>
+                <i class="iconfont icon-icon"></i>
+                <i class="iconfont icon-icon"></i>
+                <i class="iconfont icon-icon"></i>
+               </div>
+               <div class="author">家教育儿｜托德老师（郭锐） 著</div>
+               <div class="author">播音郭锐 播</div>
+               <div class="author">
+                   <span>
+                        完结全41集
+                   </span>
+                   <span>
+                        23.9万次播放
+                   </span>
+               </div>
+            </div>
+        </div>
+    	
+    	<div class="nav">
+    		<ul>
+    			<li>
+    				详情
+    			</li>
+    			<li>
+    				目录
+    			</li>
+    		</ul>
+    	</div>
+    	
+    	<div>
+    		<div class="book">
+    			<div class="book-block">
+    				<span>
+    					本课程从教育理念、情绪管理、沟通技能、共同养育四个模块全方位讲解，从根本上解决您的教育难题，帮助孩子形成良好的品格。让数百万孩子、父母和老师受益终身的经典之作。
+    				</span>
+    			</div>
+    			<div class="book-detail">
+    				<div class="book-d">
+    					<p>￥55.2/正本</p>
+    					<del>原价￥69</del>
+    					<span>限时折扣还剩4个小时</span>
+    				</div>
+    				<div>
+    					会员9折，支持听读券，高清
+    				</div>
+    			</div>
+
+    		</div>
+    		<div class="user-list"></div>
+    		
+    		
+    		<section>
+    			
+    		</section>
+    	</div>
+    	
+    	<div class="children">
+            <div class="header">
+                <h3>内容概述</h3>
+            </div>
+            <div class="content">
+            	<p>
+            		<img src="https://bookpic.lrts.me/047e08d47833460ca083f0328d290d36_100x100.jpg?19078">
+            	</p>
+            	<p>
+            		<img src="https://bookpic.lrts.me/047e08d47833460ca083f0328d290d36_100x100.jpg?19078">
+            	</p>
+            	<p>
+            		<img src="https://bookpic.lrts.me/047e08d47833460ca083f0328d290d36_100x100.jpg?19078">
+            	</p>
+            	<p>
+            		<img src="https://bookpic.lrts.me/047e08d47833460ca083f0328d290d36_100x100.jpg?19078">
+            	</p>
+            	<p>
+            		<img src="https://bookpic.lrts.me/047e08d47833460ca083f0328d290d36_100x100.jpg?19078">
+            	</p>
+            	<p>
+            		<img src="https://bookpic.lrts.me/047e08d47833460ca083f0328d290d36_100x100.jpg?19078">
+            	</p>
+            </div>
+        </div>
+    	
+    	
+    </main>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+        *{
+            margin: 0;;
+            padding: 0;
+        }
+        html{
+            font-size: 31.25vw;
+        }
+        body{
+            font-size: 16px;
+        }
+        main{
+            overflow: auto;
+            position:absolute;
+            left: 0;
+            top: 0;
+            bottom: 0;
+            right: 0;
+            padding-bottom: .50rem;
+            padding-top: .42rem;
+        }
+        .xin{
+            padding-bottom: .03rem;
+        }
+        .xin i{
+            color: #f39c11;
+            font-size: .11rem;
+        }
+        main .desc{
+            height: 1.1rem;
+            padding: .12rem .12rem .12rem .08rem;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            border-bottom: .08rem solid #f6f6f6;;
+        }
+        .desc .lr-outline{
+            width: .82rem;
+            height: 1.17rem;
+        }
+        .desc .lr-outline img{
+            width: 100%;
+            height: 100%;
+        }
+        .desc .lr-tag{
+            height: 1.17rem;
+            width: 2.08rem;
+        }
+        .desc .lr-tag h4{
+            font-weight: normal;
+        }
+        .desc .lr-tag .author{
+            color: #878787;
+            font-size: .11rem;
+            padding: .03rem 0;
+        }
+        main .nav{
+        	margin-bottom: 0.1rem;
+        }
+        main .nav ul{
+        	display: flex;
+        	justify-content: space-between;
+        	padding:0 1rem;
+        	height: 0.4rem;
+        	align-items: center;
+        }
+        main .nav ul li{
+        	list-style: none;
+        }
+        .book{
+        	border-bottom: .08rem solid #f6f6f6;;
+        }
+        .book .book-block{
+        	font-size: 0.12rem;
+        	border-bottom: 1px solid #f6f6f6;
+        	padding: 0 0.06rem;
+        	padding-bottom: 0.15rem;
+        }
+        .book-d{
+        	display: flex;
+        }
+        .book-d p{
+        	color:#f39c11;
+        }
+        .book-d span{
+        	color: #c33;
+        }
+        .book-detail .book-d del{
+        	flex: 1;
+        	margin-left: 0.1rem;
+        }
+        .book-detail div{
+        	margin: 0.06rem;
+        }
+         main .children {
+            background: #fff;
+            box-sizing: border-box;
+            border-bottom: .08rem solid #f6f6f6;
+            line-height: .26667rem;
+        }
+
+        .children .header {
+            height: 0.41rem;
+            display: flex;
+            align-items: center;
+        }
+
+        .children .header h3 {
+            border-left: 3px solid #f39c11;
+            height: 0.2rem;
+            font-size: .16rem;
+            line-height: 0.2rem;
+            padding-left: 0.06rem;
+        }
+
+        .children .header span {
+            flex: 1;
+            text-align: right;
+            padding-right: 0.06rem;
+            font-size: 0.12rem;
+            color: #f39c11;
+        }
+        .content{
+			padding:0 0.06rem;
+        }
+        .content p img{
+        	width: 100%;
+        }
+       
+
+</style>
